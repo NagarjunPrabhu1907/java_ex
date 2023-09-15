3f07a0c3d5616fe67c28465a770fa2337b4188c7
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-09-15 16:23:48 +0530

    commit msg1

---
+++ RectangleArea.java
@@ -0,0 +1,8 @@
+public class RectangleArea {
+    public static void main(String args[]) {
+        rectangle rect1 = new rectangle(5, 7);
+        int area1 = rect1.rectArea();
+        System.out.println("area1 =" + area1);
+    }
+
+}
