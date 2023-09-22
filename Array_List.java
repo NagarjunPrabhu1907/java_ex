47a8fe1459fb198b839ef7fc32a26aad98ddf7b4
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-09-22 15:49:18 +0530

    described about List

--- Array_List.java
+++ Array_List.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Array_List {
     public static void main(String[] args) {
-        List<Integer> arr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>(); // stores list of integers in the variable "arr" in empty arrayList
         arr.add(1);
         arr.add(2);
         arr.add(3);
