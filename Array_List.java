c1a8018f2cf5ed9063cb38e57af991ed315b58a5
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-09-22 15:23:31 +0530

    initial commit

---
+++ Array_List.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array_List {
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+
+        arr.add(5);
+
+        System.out.println(arr);
+    }
+
+}
