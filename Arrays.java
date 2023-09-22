79ae4644818aa149ff884722e0c17d7104932366
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-09-22 16:11:07 +0530

    message for commit

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter any 5 numbers");
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
 
+        }
+        System.out.println("the entered numbers are");
+        for (int el : arr) {
+
+            System.out.println(el);
+
+        }
     }
 }
