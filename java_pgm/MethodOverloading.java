ef065ff17b69b1432e4bf9f963b76a2765f8c879
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-09-23 15:05:44 +0530

    commits

---
+++ java_pgm/MethodOverloading.java
@@ -0,0 +1,45 @@
+package java_pgm;
+
+public class MethodOverloading {
+    static int fun(int a, int b) {
+        return a + b;
+    }
+
+    static int fun(int a) {
+        return a;
+    }
+
+    static int fun(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static void fun(String b) {
+        System.out.println(b);
+
+    }
+
+    static void fun(int a, int b, int c, int d) {
+        System.out.println(a + b + c + d);
+    }
+
+    public static void main(String args[]) {
+        int a = 60;
+        int b = 50;
+        int c = 50;
+        int d = 40;
+        String t = "ajay prince";
+
+        int fun1 = fun(a);
+        System.out.println(fun1);
+
+        int fun2 = fun(a, b);
+        System.out.println(fun2);
+
+        int fun3 = fun(a, b, c);
+        System.out.println(fun3);
+
+        fun(a, b, c);
+
+        fun(t);
+    }
+}
