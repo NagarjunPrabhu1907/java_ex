3da04efca9217120d76f87253e7a3dadb52b2def
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-10-12 14:39:42 +0530

    commits..

---
+++ java_programs/Abstraction11.java
@@ -0,0 +1,36 @@
+package java_programs;
+
+// class Person12 {
+// private double id;
+// private String name;
+
+// public Person12(){
+// id=Math.random();
+// sayHello();
+// }
+
+// private void sayHello() {
+// System.out.println("hello -" + getId());
+// }
+
+// public double getId() {
+// return id;
+// }
+
+// public String getName() {
+// return name;
+// }
+
+// public void setName(String name) {
+// this.name = name;
+// }
+// }
+
+// public class Abstraction11 {
+// public static void main(String[] args) {
+// Person12 p1 = new Person12();
+// p1.setName("Tirthal");
+// System.out.println("Person 1 -" + p1.getId() + ":" + p1.getName());
+// }
+
+// }
