b3f08e8d53f399aea664813d3fa28ce6ebc0aa41
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-10-13 16:24:05 +0530

    removing elements from list

---
+++ java_programs/LinkedLists/Linked4.java
@@ -0,0 +1,40 @@
+package java_programs.LinkedLists;
+
+import java.util.*;
+
+public class Linked4 {
+    public static void main(String args[]) {
+
+        LinkedList<String> list = new LinkedList<String>();
+
+        // Adding elements to the Linked list
+        list.add("Steve");
+        list.add("Carl");
+        list.add("Raj");
+        list.add("Negan");
+        list.add("Rick");
+
+        // Removing First element
+        // Same as list.remove(0);
+        list.removeFirst();
+
+        // Removing Last element
+        list.removeLast();
+
+        // Iterating LinkedList
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+
+        // removing 2nd element, index starts with 0
+        list.remove(1);
+
+        System.out.print("\nAfter removing second element: ");
+        // Iterating LinkedList again
+        Iterator<String> iterator2 = list.iterator();
+        while (iterator2.hasNext()) {
+            System.out.print(iterator2.next() + " ");
+        }
+    }
+}
