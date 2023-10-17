4f0a2dd296ab8577c35186a6d7fab471f85dbb9e
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-10-17 13:05:01 +0530

    implementation of LL

--- java_programs/LinkedLists/MainLinkedList.java
+++ java_programs/LinkedLists/MainLinkedList.java
@@ -5,26 +5,31 @@ import java.util.LinkedList;
 
 public class MainLinkedList {
     public static void main(String args[]) {
-        List<Integer> ll = new LinkedList<>();
-        List<Integer> al = new ArrayList<>();
+    //     List<Integer> ll = new LinkedList<>();
+    //     List<Integer> al = new ArrayList<>();
 
-        // ll.add(12);
-        // ll.add(34);
-        // ll.add(47);
+    //     // ll.add(12);
+    //     // ll.add(34);
+    //     // ll.add(47);
 
-        // ll.set(1, 88);
-        getTimeDiff(al);
-        getTimeDiff(ll);
-        // System.out.println(ll);
-    }
+    //     // ll.set(1, 88);
+    //     getTimeDiff(al);
+    //     getTimeDiff(ll);
+        
+    //     // System.out.println(ll);
+    // }
 
-    static void getTimeDiff(List<Integer> list) {
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 100000; i++) {
-            list.add(0, i);
-        }
-        long end = System.currentTimeMillis();
-        System.out.println(list.getClass().getName() + "--->" + (end - start));
+    // static void getTimeDiff(List<Integer> list) {
+    //     long start = System.currentTimeMillis();
+    //     for (int i = 0; i < 100000; i++) {
+    //         list.add(0, i);
+    //     }
+    //     long end = System.currentTimeMillis();
+    //     System.out.println(list.getClass().getName() + "--->" + (end - start));
+        NewLinkedList nll = new NewLinkedList();
+        nll.add(25);
+        nll.add(55);
+        nll.print();
     }
 
 }
