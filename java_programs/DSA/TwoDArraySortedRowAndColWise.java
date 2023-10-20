47f9a5ded7c623137ef2fce996d0022183a5459c
Nagarjun B P <118178858+NagarjunPrabhu1907@users.noreply.github.com>
2023-10-20 10:43:55 +0530

    Update TwoDArraySortedRowAndColWise.java

--- java_programs/DSA/TwoDArraySortedRowAndColWise.java
+++ java_programs/DSA/TwoDArraySortedRowAndColWise.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class TwoDArraySortedRowAndColWise {
     public static void main(String[] args) {
-        int[][] arr = {
+        int[][] arr = {              //2d array
                 {10, 20, 30, 40},
                 {15, 25, 35, 45},
                 {28, 29, 37, 49},
@@ -13,9 +13,9 @@ public class TwoDArraySortedRowAndColWise {
         System.out.println(Arrays.toString(search(arr, 33)));
     }
 
-    static int[] search(int[][] arr, int target){
-        int r = 0;
-        int c = arr[0].length - 1;
+    static int[] search(int[][] arr, int target){ //2args - 2d array and target
+        int r = 0; // first row
+        int c = arr[0].length - 1;  //last col
 
         while(r < arr.length - 1 && c > 0 ){ 
 
